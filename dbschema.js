026ac50a74c459586f7981572b98ae4e3fcf07f6
GenJay99 <jasongencarella1@gmail.com>
2019-12-19 12:47:55 -0500

    Refactor and Organize

--- dbschema.js
+++ dbschema.js
@@ -1,9 +1,9 @@
 let db = {
   screams: [
     {
-      userHandle: "user",
-      body: "this is the scream body",
-      createdAt: "2019-12-17T17:38:11.780Z",
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2019-12-17T17:38:11.780Z',
       likeCount: 5,
       commentCount: 2
     }
---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+// PUT IN DOTENV FILE IN NONEXPERIMENTAL DEPLOY
+module.exports = {
+  apiKey: 'AIzaSyAqur_oeDjnX3U4njygK9x4_ndqrm3aLg8',
+  authDomain: 'socialape-2953c.firebaseapp.com',
+  databaseURL: 'https://socialape-2953c.firebaseio.com',
+  projectId: 'socialape-2953c',
+  storageBucket: 'socialape-2953c.appspot.com',
+  messagingSenderId: '186976506229',
+  appId: '1:186976506229:web:f91b8b4b436edb0a9d833c',
+  measurementId: 'G-SBPS3FEZLR'
+};
