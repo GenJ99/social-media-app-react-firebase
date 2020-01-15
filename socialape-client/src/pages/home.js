e57a1600ed6a95ca7a1cd3295b4aea30b6c03eff
GenJay99 <jasongencarella1@gmail.com>
2020-01-15 15:33:12 -0500

    Getting Started with React

---
+++ socialape-client/src/pages/home.js
@@ -0,0 +1,14 @@
+// Snippet: rce
+import React, { Component } from 'react';
+
+export class home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>home page</h1>
+      </div>
+    );
+  }
+}
+
+export default home;
